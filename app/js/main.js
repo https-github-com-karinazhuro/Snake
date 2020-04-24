44eb8e5b26d03a4a3f993545dc271e7f06e904f0
karinazhuro <44468931+karinazhuro@users.noreply.github.com>
2020-04-24 15:21:34 +0300

    Add one segment for snake

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,20 @@
 "use strict";
 
 const field = document.getElementById('field');
-const snake = document.getElementById('snake');
+const snakeHead = document.getElementById('snakeHead');
+const snakeBody = document.getElementById('snakeBody');
+const snakeTail = document.getElementById('snakeTail');
+
 const coordinatesField = field.getBoundingClientRect();
-const coordinatesSnake = snake.getBoundingClientRect();
 
-const snakeClone = snake.cloneNode(false);
-const snake2 = snake.after(snakeClone);
-const coordinatesSnake2 = snake2.getBoundingClientRect();
-
-let intervalId;
+const snakeFullBody = [snakeHead];
+// const snakeFullBody = [snakeHead, snakeBody, snakeTail];
 
 const snakePosition = {
-    posX: 0,
-    posY: 0,
+    X: 0,
+    Y: 0,
 };
-// let posX = 0;
-// let posY = 0;
+
 const sizeSnakeForMove = 30;
 
 const keyMap = {
@@ -31,48 +29,67 @@ const directionSnake = {
     directionY: 0,
 };
 
-const snakeBody = {
-    [snake]: [coordinatesSnake.left, coordinatesSnake.top],
-    [snake2]: [coordinatesSnake2.left, coordinatesSnake2.top],
-};
-console.log(snakeBody[snake], snakeBody[snake2]);
-function moveRight() {
-    snakePosition.posX += sizeSnakeForMove;
-    snake.style.left = snakePosition.posX + 'px';
+let intervalId;
+
+function moveRight(snakeFullBody) {
+    snakePosition.X += sizeSnakeForMove;
+    snakeFullBody.style.left = snakePosition.X + 'px';
 }
 
-function moveLeft() {
-    snakePosition.posX -= sizeSnakeForMove;
-    snake.style.left = snakePosition.posX + 'px';
+function moveLeft(snakeFullBody) {
+    snakePosition.X -= sizeSnakeForMove;
+    snakeFullBody.style.left = snakePosition.X + 'px';
 }
 
-function moveTop() {
-    snakePosition.posY -= sizeSnakeForMove;
-    snake.style.top = snakePosition.posY + 'px';
+function moveTop(snakeFullBody) {
+    snakePosition.Y -= sizeSnakeForMove;
+    snakeFullBody.style.top = snakePosition.Y + 'px';
 }
 
-function moveDown() {
-    snakePosition.posY += sizeSnakeForMove;
-    snake.style.top = snakePosition.posY + 'px';
+function moveDown(snakeFullBody) {
+    snakePosition.Y += sizeSnakeForMove;
+    snakeFullBody.style.top = snakePosition.Y + 'px';
 }
 
 function getDirectionSnake() {
-    if (directionSnake.directionY === 1) {
-        moveTop();
-    } else if (directionSnake.directionY === -1) {
-        moveDown();
-    } else if (directionSnake.directionX === 1) {
-        moveRight();
-    } else if (directionSnake.directionX === -1) {
-        moveLeft();
+    for (let i = 0; i < snakeFullBody.length; i++) {
+        if (directionSnake.directionY === 1) {
+            moveTop(snakeFullBody[i]);
+        } else if (directionSnake.directionY === -1) {
+            moveDown(snakeFullBody[i]);
+        } else if (directionSnake.directionX === 1) {
+            moveRight(snakeFullBody[i]);
+        } else if (directionSnake.directionX === -1) {
+            moveLeft(snakeFullBody[i]);
+        }
     }
 }
 
+// function getDirectionSnake() {
+//     let temp = 0;
+//     // for (let i = 0; i < snakeFullBody.length; i++) {
+//         // snakeFullBody[i + 1].style.left = temp;
+//         // snakeFullBody[i + 1].style.top = temp;
+//         if (directionSnake.directionY === 1) {
+//             moveTop(snakeFullBody[i]);
+//         } else if (directionSnake.directionY === -1) {
+//             moveDown(snakeFullBody[i]);
+//         } else if (directionSnake.directionX === 1) {
+//             moveRight(snakeFullBody[0]);
+//             temp = snakeFullBody[0].style.left;
+//             snakeFullBody[1].style.left = temp;
+//             console.log(snakeFullBody[0]);
+//         } else if (directionSnake.directionX === -1) {
+//             moveLeft(snakeFullBody[i]);
+//         }
+//     // }
+// }
+
 function onFinish() {
-    if (snakePosition.posX < coordinatesField.x ||
-        snakePosition.posX > coordinatesField.right ||
-        snakePosition.posY < coordinatesField.y ||
-        snakePosition.sY > coordinatesField.bottom) {
+    if (snakePosition.X < coordinatesField.x ||
+        snakePosition.X > coordinatesField.right ||
+        snakePosition.Y < coordinatesField.y ||
+        snakePosition.Y > coordinatesField.bottom) {
         alert('game over');
         clearInterval(intervalId);
     }
@@ -81,24 +98,26 @@ function onFinish() {
 function loop() {
     getDirectionSnake();
     onFinish();
-
 }
 
-intervalId = setInterval(loop, 500);
+intervalId = setInterval(loop, 700);
 
 document.addEventListener('keydown', function (event) {
     if (event.keyCode === keyMap.arrowLeft) {
         directionSnake.directionX = -1;
         directionSnake.directionY = 0;
+        moveLeft();
     } else if (event.keyCode === keyMap.arrowRight) {
         directionSnake.directionX = 1;
         directionSnake.directionY = 0;
+        moveRight();
     } else if (event.keyCode === keyMap.arrowUp) {
         directionSnake.directionX = 0;
         directionSnake.directionY = 1;
+        moveTop();
     } else if (event.keyCode === keyMap.arrowDown) {
         directionSnake.directionX = 0;
         directionSnake.directionY = -1;
+        moveDown();
     }
 });
-
